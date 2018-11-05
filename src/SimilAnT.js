a816e56de58e8ac246db47d8782a145e43734069
David Bernhauer <david@bernhauer.cz>
2018-11-05 13:53:22 +0100

    Renamed application.

---
+++ src/SimilAnT.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './SimilAnT.scss';
+
+class SimilAnT extends Component {
+  render() {
+    return (
+      <main className="SimilAnT">
+
+      </main>
+    );
+  }
+}
+
+export default SimilAnT;
--- src/SimilAnT.test.js
+++ src/SimilAnT.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import SimilAnT from './SimilAnT';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<SimilAnT />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
